82576c05920df18b42a75d99f5aff21143e5c17f
guang16 <31829031+guang16@users.noreply.github.com>
2020-12-12 12:51:43 -0600

    Add Project update 2

--- NN.py
+++ NN.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import preprocessing, metrics
+from sklearn import preprocessing, metrics
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neural_network import MLPClassifier
@@ -18,8 +18,9 @@ X = preprocessing.StandardScaler().fit(X).transform(X)
 # Split the dataset to training 90% and testing 10%
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
 
-def do_work():
-    clf = MLPClassifier(random_state=1, max_iter=300)
+
+def do_work(hiddenLayer, activation):
+    clf = MLPClassifier(hidden_layer_sizes=hiddenLayer, activation=activation, random_state=1, max_iter=3000)
     clf.fit(X_train, y_train)
     # Test out-of-sample test set
     y_pred = clf.predict(X_test)
@@ -27,13 +28,20 @@ def do_work():
     print(metrics.classification_report(y_test, y_pred, digits=3))
     print('accuracy', accuracy_score(y_test, y_pred))
     # Calculate cv score with 'roc_auc_ovr' scoring and 10 folds
-    accuracy = cross_val_score( clf, X, y, scoring='roc_auc_ovr', cv=10)
+    accuracy = cross_val_score(clf, X, y, scoring='roc_auc_ovr', cv=10)
     print('cross validation score with roc_auc_ovr scoring', accuracy.mean())
     # Calculate roc_auc score with multiclass parameter
-    print('roc_auc_score', roc_auc_score(y_test,  clf.predict_proba(X_test), multi_class='ovr'))
-
-do_work()
+    print('roc_auc_score', roc_auc_score(y_test, clf.predict_proba(X_test), multi_class='ovr'))
 
 
+print('1 layer, 100 hidden nodes, logistic activation function')
+do_work(100, 'logistic')
+print('End Scenario 1')
 
+print('1 layer, 100 hidden nodes, RELU activation function')
+do_work(100, 'relu')
+print('End Scenario 2')
 
+print('3 Layer, 100 hidden nodes, logistic activation function')
+do_work((100, 100, 100), 'logistic')
+print('End Scenario 3')
